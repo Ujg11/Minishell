64e86c9a7cf25ff92bb62e04a5d7c2b24e6cf177
Oriol Jimenez Garrich <ojimenez@car11s4.42barcelona.com>
2024-02-09 13:59:25 +0100

    Comillas controladas

--- src/commands/echo.c
+++ src/commands/echo.c
@@ -6,11 +6,47 @@
 /*   By: ojimenez <ojimenez@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 11:42:36 by agrimald          #+#    #+#             */
-/*   Updated: 2024/02/08 16:33:20 by ojimenez         ###   ########.fr       */
+/*   Updated: 2024/02/09 13:54:40 by ojimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+char	*fill_new(char *old, char *new)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (old[i])
+	{
+		if (old[i] != '\'')
+		{
+			new[j] = old[i];
+			j++;
+		}
+		i++;
+	}
+	new[j] = '\0';
+	return (new);
+}
+
+char	*quit_comillas(char *str)
+{
+	int		len;
+	char	*new;
+
+	len = ft_strlen(str);
+	if (str[0] == '\'')
+	{
+		new = malloc((len - 1) * sizeof(char));
+		new = fill_new(str, new);
+		free(str);
+		return (new);
+	}
+	return (str);
+}
 
 int	ft_echo(char **argv)
 {
@@ -26,6 +62,7 @@ int	ft_echo(char **argv)
 	}
 	while (argv[i])
 	{
+		argv[i] = quit_comillas(argv[i]);
 		printf("%s", argv[i]);
 		if (argv[i + 1])
 			printf(" ");
