1e027d8217b64bbad4af291213229505a2acae6f
Phroms <hernangrim.10@gmail.com>
2024-02-09 21:21:50 +0100

    unset

--- src/commands/unset.c
+++ src/commands/unset.c
@@ -6,7 +6,7 @@
 /*   By: agrimald <agrimald@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 18:52:41 by agrimald          #+#    #+#             */
-/*   Updated: 2024/02/09 16:40:12 by agrimald         ###   ########.fr       */
+/*   Updated: 2024/02/09 21:21:27 by agrimald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,9 @@ int	ft_unset(char *variable, t_env *env)
 
 	i = 0;
 	env_ptr = env->env_cpy;
+
+	if (variable == NULL)
+		return (0);
 	while (env_ptr[i] != NULL)
 	{
 		if (ft_strncmp(env_ptr[i], variable, ft_strlen(variable)) == 0)
